de91975e5a68c430b73ba0c34019d1c688b28d9b
sachin kumar <sachinmehtab@gmail.com>
2025-03-13 21:09:56 +0530

    Initial Commit

---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by sachin kumar on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
